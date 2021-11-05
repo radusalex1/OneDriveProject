41fbc84465a97fc4593396297447d2f4ae895e2a
radusalex1 <54530468+radusalex1@users.noreply.github.com>
2021-11-05 14:54:47 +0300

    deletes .vs folder

--- OneDrive/OneDrive.cpp
+++ OneDrive/OneDrive.cpp
@@ -1,6 +1,12 @@
+//include the below additional libraries
 #include <iostream>
+#include <windows.h>
+#include <sqlext.h>
+#include <sqltypes.h>
+#include <sql.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << " Hello World!\n";
+    
 }
