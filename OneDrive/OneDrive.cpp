6541f3284db542daab1c2067aeee7bab4ef86deb
radusalex1 <54530468+radusalex1@users.noreply.github.com>
2021-11-07 14:18:44 +0300

    Update OneDrive.cpp

--- OneDrive/OneDrive.cpp
+++ OneDrive/OneDrive.cpp
@@ -13,7 +13,6 @@ void OneDrive::on_pushButton_login_clicked()
     QString username = ui.line_username->text();
     QString password = ui.line_password->text();
  
-
     if (username == "test" && password == "pass")
     {
         QMessageBox::information(this, "Login", "username and password correct");
@@ -21,9 +20,7 @@ void OneDrive::on_pushButton_login_clicked()
     else
     {
         QMessageBox::information(this, "Login", "username and password incorrect");
-
     }
-
 }
 
 void OneDrive::on_Close_clicked()
