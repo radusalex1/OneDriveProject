9692fbc370d047eced9e0616eb3a56052c3cb122
MirceaVartolomei <vartolomeimircea30@yahoo.com>
2021-11-21 13:42:55 +0200

    [ADD] File explorer header

---
+++ OneDrive/onedrive_file_explorer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <QtWidgets/QWidget>
+#include <QtCore>
+#include <QtGui>
+#include "ui_onedrive_file_explorer.h"
+
+class OneDriveFileExplorer : public QWidget
+{
+    Q_OBJECT
+
+public:
+    OneDriveFileExplorer(QWidget* parent = Q_NULLPTR);
+private slots:
+
+private:
+    Ui::fileExplorerUI ui;
+    QFileSystemModel* dirmodel;
+    QFileSystemModel* filemodel;
+};
