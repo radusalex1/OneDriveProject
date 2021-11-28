00dc9d4c809f65a0932e61fec29f9761013854b4
radusalex1 <54530468+radusalex1@users.noreply.github.com>
2021-11-28 13:33:45 +0300

    [ADD] client class

---
+++ OneDrive/Client.cpp
@@ -0,0 +1,10 @@
+#include "Client.h"
+
+void Client::sendFiles(std::string path)
+{
+}
+
+void Client::getFiles(std::string path)
+{
+
+}
---
+++ OneDrive/Client.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <string>
+class Client
+{
+public:
+	void sendFiles(std::string path);
+	void getFiles(std::string path);
+
+};
+
