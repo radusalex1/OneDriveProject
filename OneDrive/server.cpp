027f9dc93d79871041e8b10c13ff01523022b1e8
stefgore12 <stroiestefanvlad2001@gmail.com>
2021-11-28 11:59:23 +0200

    [ADD] Server init method

--- OneDrive/server.cpp
+++ OneDrive/server.cpp
@@ -14,3 +14,13 @@ void CTcpListener::Send(int clientSocket, std::string msg)
 {
 	send(clientSocket, msg.c_str(), msg.size() + 1, 0);
 }
+
+bool CTcpListener::Init()
+{
+	WSADATA data;
+	WORD version = MAKEWORD(2, 2);
+
+	int wsInit = WSAStartup(version, &data);
+	
+	return wsInit == 0;
+}
