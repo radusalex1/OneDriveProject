037f219007e65215264a0ea583233222e0e654d8
radusalex1 <54530468+radusalex1@users.noreply.github.com>
2021-11-28 13:58:01 +0300

    [ADD]get files method

--- OneDrive/Client.cpp
+++ OneDrive/Client.cpp
@@ -3,7 +3,11 @@
 #include <fstream>
 #include <string>
 #include <WS2tcpip.h>
-
+/// <summary>
+/// get is for get file from server to client given the path from the server
+/// send is for send files from client to server given the 
+/// </summary>
+/// <param name="path"></param>
 void Client::sendFiles(std::string path)
 {
 	std::string ipAddress = "127.0.0.1";  //ipadress
@@ -63,8 +67,7 @@ void Client::sendFiles(std::string path)
 	std::ifstream file;
 	char fileRequested[FILENAME_MAX];
 
-	std::cout << "Enter file name: " << std::endl;
-	std::cin.getline(fileRequested, FILENAME_MAX);
+	strcpy(fileRequested, path.c_str());
 
 	file.open(fileRequested, std::ios::binary);
 
@@ -99,5 +102,95 @@ void Client::sendFiles(std::string path)
 
 void Client::getFiles(std::string path)
 {
+	std::string ipAddress = "127.0.0.1";  //ipadress
+	int port = 54000;					  //listening port
+
+	///initialize winsock
+	WSADATA data;
+	WORD ver = MAKEWORD(2, 2);
+	int wsResult = WSAStartup(ver, &data);
+
+	if (wsResult != 0)
+	{
+		std::cerr << "cant start winsock" << wsResult << std::endl;
+		WSACleanup();
+		return;
+	}
+	///create a socked
+
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET)
+	{
+		std::cerr << "cant create socket. err#" << WSAGetLastError() << std::endl;
+		return;
+	}
+
+	/// fill in a hint structure
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(port);
+	inet_pton(AF_INET, ipAddress.c_str(), &hint.sin_addr);
+
+	/// connect to server
+	int connResult = connect(sock, (sockaddr*)&hint, sizeof(hint));
+	if (connResult == SOCKET_ERROR)
+	{
+		std::cerr << "cant con to server, err#" << WSAGetLastError() << std::endl;
+		closesocket(sock);
+		WSACleanup();
+		return;
+	}
+	char buf[32768];
+	std::string userInput;
+	std::cout << "1-get, 2-send" << std::endl;
+	userInput = "get";
+
+	std::ofstream file;
+	char fileRequested[FILENAME_MAX];
+	int fileDownloaded = 0;
+	long fileRequestedSize = 0;
+
+	strcpy(fileRequested, path.c_str());
+
+	int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+
+	if (sendResult == SOCKET_ERROR)
+	{
+		std::cerr << "error\n";
+	}
+	else
+	{
+		std::cerr << "server> i'm sending the file" << std::endl;
+	}
+	sendResult = send(sock, fileRequested, FILENAME_MAX, 0);
+
+	if (sendResult != SOCKET_ERROR)
+	{
+		int bytesReceived = recv(sock, (char*)&fileRequestedSize, sizeof(long), 0);
+		ZeroMemory(buf, 4096);
+		//bytesReceived = recv(sock, buf, 32768, 0);
+
+		if (bytesReceived > 0)
+		{
+			file.open(fileRequested, std::ios::binary, std::ios::trunc);
+			do {
+				memset(buf, 0, 4096);
+				bytesReceived = recv(sock, buf, 4096, 0);
+				if (bytesReceived == 0 || bytesReceived == -1)
+				{
+					break;
+				}
+				file.write(buf, bytesReceived);
+				fileDownloaded += bytesReceived;
+			} while (fileDownloaded < fileRequestedSize);
+			file.close();
+		}
+
+	}
+	else
+	{
+		std::cerr << "file not found\n";
+	}
+
 
 }
