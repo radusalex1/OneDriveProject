b37f3bf94b085aa6f0aff167c6545bf459f62707
Smeurian <smeutudor4788@gmail.com>
2022-01-13 15:16:46 +0200

    [CHANGE] Add smart naming to onedrive.cpp

--- OneDrive/OneDrive.cpp
+++ OneDrive/OneDrive.cpp
@@ -11,7 +11,7 @@
 OneDrive::OneDrive(QWidget *parent)
     : QWidget(parent)
 {
-    ui.setupUi(this);
+    m_ui.setupUi(this);
     QPixmap background("BackgroundLoginImg.jpg");
     background = background.scaled(this->size(), Qt::IgnoreAspectRatio);
     QPalette palette;
@@ -19,9 +19,9 @@ OneDrive::OneDrive(QWidget *parent)
     this->setPalette(palette);
 
     QPixmap folderpic("Logo.png");
-    int width = ui.label_picture->width();
-    int height = ui.label_picture->height();
-    ui.label_picture->setPixmap(folderpic.scaled(width, height, Qt::KeepAspectRatio));
+    int width = m_ui.label_picture->width();
+    int height = m_ui.label_picture->height();
+    m_ui.label_picture->setPixmap(folderpic.scaled(width, height, Qt::KeepAspectRatio));
 
     this->setWindowIcon(QIcon("Logo.png"));//Generate window icon
     
@@ -29,8 +29,8 @@ OneDrive::OneDrive(QWidget *parent)
 
 void OneDrive::on_pushButton_login_clicked()
 {
-    std::string username = ui.line_username->text().toStdString();
-    std::string password = ui.line_password->text().toStdString();
+    std::string username = m_ui.line_username->text().toStdString();
+    std::string password = m_ui.line_password->text().toStdString();
     DataBaseConnect *dbc = new DataBaseConnect();
 
     if (dbc->isUser(username,password)) 
@@ -51,16 +51,16 @@ void OneDrive::on_pushButton_login_clicked()
 
 OneDrive::~OneDrive()
 {
-    delete[] onedriveRegister;
+    delete[] m_onedriveRegister;
 }
 
 void OneDrive::on_pushButton_register_clicked()
 {
     this->hide();
-    onedriveRegister = new OneDriveRegister();
-    onedriveRegister->setWindowTitle("OneDrive - Register");
-    onedriveRegister->setWindowFlags(Qt::Window);
-    onedriveRegister->setAttribute(Qt::WA_DeleteOnClose, true);   //delete itself on closing
-    QObject::connect(onedriveRegister, SIGNAL(destroyed(QObject*)), this, SLOT(show()));
-    onedriveRegister->show(); 
+    m_onedriveRegister = new OneDriveRegister();
+    m_onedriveRegister->setWindowTitle("OneDrive - Register");
+    m_onedriveRegister->setWindowFlags(Qt::Window);
+    m_onedriveRegister->setAttribute(Qt::WA_DeleteOnClose, true);   //delete itself on closing
+    QObject::connect(m_onedriveRegister, SIGNAL(destroyed(QObject*)), this, SLOT(show()));
+    m_onedriveRegister->show(); 
 }
