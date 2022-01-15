18caba294885dcaf78d28ebe3f961cecfe5a2f93
stefgore12 <stroiestefanvlad2001@gmail.com>
2022-01-15 15:43:02 +0200

    [ADD] Choose path method

--- OneDrive/onedrive_register.h
+++ OneDrive/onedrive_register.h
@@ -12,6 +12,7 @@ public:
 private slots:
     void on_pushButton_registerMethod_clicked();
     void on_pushButton_GoBack_clicked();
+    void on_pushButton_ChoosePath_clicked();
 
 private:
     Ui::registerUI m_ui;
